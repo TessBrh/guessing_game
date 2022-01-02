d72a693fbe8a2ec6c7d12e9cb182e1f257750449
ThePoopLord <88780282+ThePoopLord@users.noreply.github.com>
2022-01-02 17:23:52 +0200

    Update source_code.cpp

--- source_code.cpp
+++ source_code.cpp
@@ -1,21 +1,40 @@
 #include <iostream>
+#include <ctype.h>
 
 using namespace std;
 
 int main() {
     int randomNumber = rand() % 21;
-    int userGuess;
+    int Logs[20];
+    string userInput;
+    userInput = (tolower(userInput));
+    int userGuess = stoi(userInput);
     cout << "Guess a number between 0 and 20: ";
     cout << randomNumber;
     for (int Tries = 1; randomNumber != userGuess;) {
-        int Logs[20];
-        cin >> userGuess;
-        int Logs[Tries] = userGuess;
+        cin >> userInput;
+        Logs[Tries] = userInput;
+        switch(userInput) {
+            case "restart":
+            
+            break;
+            case "logs":
+            
+            break;
+            case "stop":
+            
+            break;
+            default:
+            break;
+        }
         if (userGuess == randomNumber) {
            cout << "Correct!" << "\n";
         }
         else {
             cout << "wrong xd, the number was: " << randomNumber << "\n";
+            if (userGuess != Logs[Tries]) {
+                Tries++;
+            }
         }
     }
     
